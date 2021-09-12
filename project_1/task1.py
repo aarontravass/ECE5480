07b085d7ce9eba600296fc7ae89087bb2aea03fa
aarontravass <aaron.travass@hotmail.com>
2021-09-13 02:46:28 +0530

    Reformatted

--- project_1/task1.py
+++ project_1/task1.py
@@ -1,5 +1,5 @@
-
 import requests
+
 
 def getPageRequests(url):
     user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
@@ -8,5 +8,7 @@ def getPageRequests(url):
     file = open('FILE.txt', 'w')
     file.write(result.text)
     file.close()
-url=input("Enter a url = ")
+
+
+url = input("Enter a url = ")
 getPageRequests(url)
--- project_1/task2.py
+++ project_1/task2.py
@@ -5,6 +5,6 @@ user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 
 headers = {'User-Agent': user_agent}
 myres = requests.get("https://www.orionscache.com/books", headers=headers)
 soup = BeautifulSoup(myres.text, "html.parser")
-links=soup.find_all('a')
+links = soup.find_all('a')
 for link in links[:10]:
     print(link.get('href'))

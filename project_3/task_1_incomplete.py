043c2074d1f7e76990c2e4d694ab6588d081f45a
aarontravass <aaron.travass@hotmail.com>
2021-10-19 11:23:02 +0530

    Project 3 bugs

--- project_3/task_1_incomplete.py
+++ project_3/task_1_incomplete.py
@@ -11,27 +11,33 @@ files = []
 dirs = []
 
 for item in directory_contents:
-    if(os.path.isfile(item)):
-        files.append(item)
-    elif(os.path.isdir(item)):
-        dirs.append((item))
+    path=os.path.join(base_directory,item)
+    if(os.path.isfile(path)):
+        files.append(path)
+    elif(os.path.isdir(path)):
+        dirs.append((path))
 
+if(len(files)==0):
+    print("No files found")
+else:
+    outfile = open("files.txt", "a")
+    print("Following are the files found")
+    for file in files:
+        print(file)
+        outfile.write(file+"\n")
+    print("\n")
+    outfile.close()
 
-outfile = open("files.txt", "a")
-print("Following are the files found")
-for file in files:
-    print(base_directory+"\\"+file)
-    outfile.write(base_directory+"\\"+file+"\n")
-print("\n")
-outfile.close()
-outfile = open("directors.txt", "a")
-
-print("Following are the directories found")
-for dir in dirs:
-    print(base_directory+"\\"+dir)
-    outfile.write(base_directory + "\\" + dir+"\n")
-# write your found files to a text file
-outfile.close()
+if(len(dirs)==0):
+    print("No directories found")
+else:
+    outfile = open("directors.txt", "a")
+    print("Following are the directories found")
+    for dir in dirs:
+        print(dir)
+        outfile.write(dir+"\n")
+    # write your found files to a text file
+    outfile.close()
 # write your found directories to a text file
 
   
